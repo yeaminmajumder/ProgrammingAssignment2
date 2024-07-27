2d2f75886493e2c9248ae389ce6ccbfeec8f3dbd
Yeamin Majumder <yeaminmajumder96@gmail.com>
2024-07-27 13:16:52 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        mat <- NULL
+        inv_cache <- NULL
 
+        setMatrix <-function(x){
+                mat <<- x
+                inv_cache <<- NULL
+                }
+
+        getInverse <- function() {
+                if (is.mull(inv_cache)){
+                        inv_cache <<- solve(mat)
+                        }
+                inv_cache
+                }
+
+list(setMatrix = setMatrix, getInverse =getInverse)
+        }
+cacheSolve <- function(x, ...){
+        inv_cache <- x$getInverse()
+        inv_cache
+        }
 }
 
 
